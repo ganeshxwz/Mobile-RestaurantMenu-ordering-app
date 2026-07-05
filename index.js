bdb1c295ca01e1fc701ecd3e050f32fb35fb11cb
ganeshxwz <ganeshxwz@gmail.com>
2026-07-05 17:38:47 +0530

    commit for fixing github action pages

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import { menuArray } from "./data.js"
 const menu = document.getElementById("menu")
 
 
+document.addEventListener("click", function(e){
+    if(e.target.dataset.id){
+        handleaddeditem(e.target.dataset.id)
+    }
+})
+
+function handleaddeditem(itemid){
+    const targetitem = menuArray.filter(function(item){
+        return item.id === itemid
+    })
+}
 
 
 
 
-
-
-
-
+function order(){
+    
+}
 
 
 
@@ -24,7 +34,7 @@ function getmenuhtml(menuarr){
                 <p class="details ingredients">${item.ingredients}</p>
                 <h4 class="details">$${item.price}</h4>
             </div> 
-            <button class="addbtn">+</button>   
+            <button class="addbtn" data-id="${item.id}">+</button>   
         </div>`
     }).join('');
     return html  
